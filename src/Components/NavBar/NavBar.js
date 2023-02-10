d65b1a8c228a02942a4c4b8a1e8ab004f54191e1
preye omusuku <omusukup@yahoo.com>
2023-02-10 19:03:52 +0100

    adjusted some features in my portfolio

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React,{useState}from 'react'
 import { HashLink as Link } from 'react-router-hash-link'
-import {Newspaper, Home, Work,Person, MessageOutlined} from '@mui/icons-material'
+import {Newspaper, Home, Work,Person,ContactMail} from '@mui/icons-material'
 import classes from'../NavBar/NavBar.modules.css'
 
 
@@ -11,11 +11,11 @@ const NavBar = () => {
   return (
     <nav className={classes.nav}>
      
-    <Link to='#' aria-label='home'onClick ={(() => setActiveNav('#portfolio'))}className= {activeNav === '#portfolio' ? 'active' : ''}><Home /></Link>
-    <Link to='#about' aria-label='about'onClick={(() => setActiveNav('#about'))} className= {activeNav === '#about' ? 'active' : ''}><Person /></Link>
-    <Link to='#projects'aria-label='project' onClick={(() => setActiveNav('#projects'))} className= {activeNav === '#projects' ? 'active' : ''}><Work /></Link>
-    <Link to='#blog' aria-label='blog'onClick={(() => setActiveNav('#blog'))} className= {activeNav === '#blog' ? 'active' : ''}><Newspaper /></Link>
-    <Link to='#contact' aria-label='contact'onClick={(() => setActiveNav('#contact'))} className= {activeNav === '#contact' ? 'active' : ''}><MessageOutlined /></Link>
+    <Link to='#portfolio' smooth aria-label='home'onClick ={(() => setActiveNav('#portfolio'))}className= {activeNav === '#portfolio' ? 'active' : ''}><Home /></Link>
+    <Link to='#about'  smooth aria-label='about'onClick={(() => setActiveNav('#about'))} className= {activeNav === '#about' ? 'active' : ''}><Person /></Link>
+    <Link to='#projects' smooth aria-label='project' onClick={(() => setActiveNav('#projects'))} className= {activeNav === '#projects' ? 'active' : ''}><Work /></Link>
+    <Link to='#blog' smooth aria-label='blog'onClick={(() => setActiveNav('#blog'))} className= {activeNav === '#blog' ? 'active' : ''}><Newspaper /></Link>
+    <Link to='#contact' smooth aria-label='contact'onClick={(() => setActiveNav('#contact'))} className= {activeNav === '#contact' ? 'active' : ''}><ContactMail /></Link>
 
     
     </nav>
